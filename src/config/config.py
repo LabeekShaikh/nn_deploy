a81229446184611c742fc535751c6a6fbd2c59b0
LabeekShaikh <labeekshaikh393@gmail.com>
2024-06-02 10:44:26 +0530

    Modified data_management.py and added preproessors.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,14 +1,20 @@
+import os
+import pathlib
+import src
 NUM_INPUTS = 2
 NUM_LAYERS = 3
 P = [NUM_INPUTS]
-
-
 
 f = [None,"linear","sigmoid"]
 
 LOSS_FUNCTION = "Mean Squared Error"
 MINI_BATCH_SIZE = 1
 
+PACKAGE_ROOT = pathlib.Path(src.__file__).reslove().parent
+DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
+#"/src/datastes"
+SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_model")
+#"/src/trained_models"
 
 #theta0 = [None]
 #theta = [None]
